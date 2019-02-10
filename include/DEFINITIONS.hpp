40d9f72028f0e466e0531d04064c2d29dca50b85
Tungadri <cs17btech11043@iith.ac.in>
2019-02-10 23:49:34 +0530

    changed path of resources for menu

--- include/DEFINITIONS.hpp
+++ include/DEFINITIONS.hpp
@@ -5,7 +5,7 @@
 
 #define SPLASH_STATE_SHOW_TIME 3.0
 
-#define SPLASH_SCENE_BACKGROUND_FILEPATH "Resources/res/Splash Background.png"
-#define MAIN_MENU_BACKGROUND_FILEPATH "Resources/res/Menu Background.png"
-#define GAME_TITLE_FILEPATH "Resources/res/title.png"
-#define PLAY_BUTTON_1_FILEPATH "Resources/res/PlayButton.png"
+#define SPLASH_SCENE_BACKGROUND_FILEPATH "res/Splash Background.png"
+#define MAIN_MENU_BACKGROUND_FILEPATH "res/Menu Background.png"
+#define GAME_TITLE_FILEPATH "res/title.png"
+#define PLAY_BUTTON_1_FILEPATH "res/PlayButton.png"
