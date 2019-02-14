f01e7226da67cc6444c1dbe5ecae94d7e1912176
Tungadri <33343120+Tungadri@users.noreply.github.com>
2019-02-14 21:31:20 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,19 +1,9 @@
-#include "SFML/Graphics.hpp"
-#include "Box2D/Box2D.h"
-#include "player.h"
-#include "game.h"
-#include <bits/stdc++.h>
-using namespace std;
-#define DEGTORAD 0.0174532925199432957f
-#define RADTODEG 57.295779513082320876f
-
-const float SCALE = 30.f;
+#include "Game.hpp"
+#include "DEFINITIONS.hpp"
 
 int main()
-{   
-    Game gameState;
-    //creating ground
-    gameState.ground = gameState.createGround(b2Vec2(400.f/SCALE, 550.f/SCALE));
-    gameState.gameLoop();
-    return 0;
+{
+	stickman::Game2(SCREEN_WIDTH, SCREEN_HEIGHT, "Stickman Fighter");
+
+	return EXIT_SUCCESS;
 }
