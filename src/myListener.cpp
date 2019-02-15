d6e1c5ea2dc39f34b1ba9051c41227b079a693a9
Shivashish <shivashishsuman.23@gmail.com>
2019-02-15 08:11:27 +0530

    added impulse

--- src/myListener.cpp
+++ src/myListener.cpp
@@ -16,5 +16,13 @@ void myListener::BeginContact(b2Contact* contact)
     if(contact->IsTouching() == true && ( (a>=1 && a<=4 && b>=5 && b<=8) || ( a>=5 && a<=8 && b>=1 && b<=4 ) ) )
     {    
         this->Queue.push(std::make_pair(a,b));
+        b2WorldManifold worldManifold;
+        contact->GetWorldManifold(&worldManifold);
+        b2Body* bodya = contact->GetFixtureA()->GetBody();
+        b2Body* bodyb = contact->GetFixtureB()->GetBody();
+        b2Vec2 impulse = (10*worldManifold.normal.x,10*worldManifold.normal);
+        b2Vec2 impulse2 = (-10*worldManifold.normal.x,-10*worldManifold.normal);
+        bodya->ApplyLinearImpulse(impulse,bodya->GetWorldCenter(),true);
+        bodyb->ApplyLinearImpulse(impulse2,bodyb->GetWorldCenter(),true);
     }
 }
