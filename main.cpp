cd81d273f6bbc1ee1767933673ce00f23fa0cf15
Tungadri <33343120+Tungadri@users.noreply.github.com>
2019-02-27 21:23:14 +0530

    Changed the main.cpp and cmake for including gtest
    #include tests.hpp is giving segmentation fault if not running tests so it is commented.
    Don't uncomment it for now

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include "Game.hpp"
+#include <gtest/gtest.h>
+// #include "tests.hpp"
 #include "DEFINITIONS.hpp"
 
-int main()
+int main(int argc,char **argv)
 {
+	//testing::InitGoogleTest(&argc, argv);
+    //return RUN_ALL_TESTS();
+    
 	stickman::Game2(SCREEN_WIDTH, SCREEN_HEIGHT, "Stickman Fighter");
 
 	return EXIT_SUCCESS;
