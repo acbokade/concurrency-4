a448bf9be5afd5882a399462043a93169736bd06
acbokade <acbokade@gmail.com>
2019-02-28 13:31:35 +0530

    changes in mainGame.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include "Game.hpp"
 #include <gtest/gtest.h>
-//#include "tests.hpp"
+#include "tests.hpp"
 #include "DEFINITIONS.hpp"
 
 int main(int argc,char **argv)
 {
-	//testing::InitGoogleTest(&argc, argv);
-    //return RUN_ALL_TESTS();
+	testing::InitGoogleTest(&argc, argv);
+   	int a = RUN_ALL_TESTS();
     
 	stickman::Game2(SCREEN_WIDTH, SCREEN_HEIGHT, "Stickman Fighter");
 
